9abcfc7082a6299a905c3380bd0fa7773527ce80
Fateen Najib Indramustika <70865641+Teenjb@users.noreply.github.com>
2022-07-09 20:44:29 +0700

    ready to deploy

--- oh-ffui/config/database.js
+++ oh-ffui/config/database.js
@@ -1,6 +1,32 @@
 const path = require('path');
+const parse = require('pg-connection-string');
 
-module.exports = ({ env }) => ({
+module.exports = ({ env }) => {
+  
+  if(env('NODE_ENV') === 'production') {
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgresql',
+            host: config.host,
+            port: config.port,
+            user: config.user,
+            password: config.password,
+            database: config.database
+          },
+          options: {
+            ssl: false
+          }
+        }
+      }
+    };
+  }
+
+  return{
   connection: {
     client: 'sqlite',
     connection: {
@@ -8,4 +34,4 @@ module.exports = ({ env }) => ({
     },
     useNullAsDefault: true,
   },
-});
+}};
