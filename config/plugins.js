7fd4f0bd643b7d17df9d81200dfe106d8fcedbc1
Fateen Najib Indramustika <70865641+Teenjb@users.noreply.github.com>
2022-07-09 21:07:19 +0700

    fix deploy issue

--- config/plugins.js
+++ config/plugins.js
@@ -5,9 +5,8 @@ module.exports = ({ env }) => {
                 provider: 'aws-s3',
                 providerOptions: {
                     accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                    secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+                    secretAccessKey: env('AWS_ACCESS_SECRET'),
                     region: env('AWS_REGION'),
-                    bucket: env('AWS_BUCKET'),
                     params: {
                         Bucket: env('AWS_BUCKET')
                     }
