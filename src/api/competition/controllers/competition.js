b3de74584b135bbddc216bbbb9cd89fc625bd1b6
Muhammad Raihan Wijaya <raihanwijaya77@gmail.com>
2022-07-26 19:21:10 +0700

    Finished Submission
    Submission untuk competition sudah selesai

--- src/api/competition/controllers/competition.js
+++ src/api/competition/controllers/competition.js
@@ -10,6 +10,38 @@ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 module.exports = createCoreController('api::competition.competition', {
     async create(ctx) {
         const { user } = ctx.state;
-        
+
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            
+            const checkCompetition = await strapi.db.query('api::competition.competition').findMany({
+                where: {
+                    users_permissions_user: user.id,
+                    competitionType: data.competitionType
+                }
+            });
+
+            console.log(checkCompetition);
+
+            if (checkCompetition.length > 0) {
+                return ctx.send({
+                    status: 'Competition already exists'
+                });
+            }
+            else {
+                const inputCompetition = await strapi.service('api::competition.competition').create({ data, files });
+
+                const updateCompetition = await strapi.entityService.update('api::competition.competition', inputCompetition.id, {
+                    data: {
+                        users_permissions_user: user.id
+                    },
+                });
+    
+                return ctx.send({
+                    entry: updateCompetition,
+                    status: 'Competition created'
+                })
+            }
+        }
     }
 });
